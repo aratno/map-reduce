8ea20b8a6eeb314e72f29bdefad784b24a35dcea
Abe Ratnofsky <aratno@gmail.com>
2017-12-11 16:13:30 -0600

    Added basic parallel implementation using fork()

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int INPUT_SIZE     = 3;
 int INPUT_VECTOR[] = {0, 10, 12};
@@ -15,8 +16,48 @@ int* map(int* items, int size, int (*f)(int)) {
 
     int* start = malloc(sizeof(int) * size);
 
-    for(int i = 0; i < size; i++)
+    for(int i = 0; i < size; i++) {
+
+        int fds[] = {0, 0};
+        pipe(fds);
+
+        pid_t pid = fork();
+        if( pid != 0 ){
+            // Parent
+            printf("Process %d spawned child %d with PID: %d\n", getpid(), i, pid);
+            printf("Main process writing to pipe...\n");
+            close(fds[0]);
+
+            int outbound = i;
+            write(fds[1], &outbound, sizeof(int));
+
+            int raw_status = 0;
+            waitpid(pid, &raw_status, 0);
+            int exit_status = WEXITSTATUS(raw_status);
+
+            printf("Main process saw process %d terminate with status %d\n", pid, exit_status);
+            start[i] = exit_status;
+
+        } else {
+            // Child
+
+            pid = getpid();
+            close(fds[1]);
+
+            int inbound = 0;
+            read(fds[0], &inbound, sizeof(int));
+
+            printf("Child process %d read: %d\n", pid, inbound);
+
+            int result = f(items[inbound]);
+            printf("Child process %d exiting with result: %d\n", pid, result);
+            exit(result);
+
+            return NULL;
+        }
+
         start[i] = f(items[i]);
+    }
 
     return start;
 }
@@ -39,14 +80,16 @@ int sum(int a, int b) {
 }
 
 int main() {
-    print_items(INPUT_VECTOR, INPUT_SIZE);
+    pid_t main_pid = getpid();
+    printf("Main process has PID: %d\n", main_pid);
 
-    int* mapped_items = map(INPUT_VECTOR, INPUT_SIZE, twice);
-    print_items(mapped_items, INPUT_SIZE);
+    int* mapped = map(INPUT_VECTOR, INPUT_SIZE, twice);
 
-    int reduced_item = reduce(mapped_items, INPUT_SIZE, sum, 0);
-    printf("%d\n", reduced_item);
+    printf("Mapped: ");
+    print_items(mapped, INPUT_SIZE);
 
-    free(mapped_items);
+    int reduced = reduce(mapped, INPUT_SIZE, sum, 0);
+    printf("Reduced: %d\n", reduced);
+
     return 0;
 }
